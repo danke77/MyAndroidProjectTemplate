cd9eefd6d989586f32201903f9676b6fbbd9a068
danke <xue_xiaozhe@163.com>
2018-06-13 14:38:18 +0800

    router

---
+++ lib_common/src/main/java/com/danke/android/template/common/Router.kt
@@ -0,0 +1,20 @@
+package com.danke.android.template.common
+
+/**
+ * @author danke
+ * @date 2018/6/13
+ */
+interface Router {
+
+    interface A {
+
+    }
+
+    interface B {
+
+    }
+
+    interface C {
+
+    }
+}
